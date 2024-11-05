2881cee7f75a825cc2e93326b7e5586fc10e4792
Aryan-017-Y <aryan.23cs017@sode-edu.in>
2024-11-05 15:43:12 +0530

    Add files via upload

---
+++ m1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class m1 {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            System.out.println(i*2+ " ");
+        }
+    System.out.println("");
+    for(int i=0;i<=5;i++) {
+        System.out.print(i*2+1+" ");
+    }
+    System.out.println("");
+    for(int j=3;j<10;j++) {
+        for(int i=0;i<=10;i++) {
+            System.out.print(i*j+" ");
+        }
+        System.out.println();
+    }
+    }
+}
